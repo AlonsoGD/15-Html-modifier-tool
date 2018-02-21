a31cb03799f3769e11a0d4406f6c94f8df031781
AlonsoGD <dani.alonso.garcia@gmail.com>
2018-02-21 11:10:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ var anchors;
 var paras;
 var images;
 
-var modHrefsValues = [];
-var modSrcsValues = [];
-var modAltsValues = [];
-var modTitlesValues = [];
+var textToSave;
+
+var file;
+var contents;
 
 function readHTML(evt) {
   //Retrieve the first (and only!) File from the FileList object
-  var f = evt.target.files[0];
-  var contents;
-  console.log(f);
-  if (f) {
+  file = evt.target.files[0];
+  console.log(file);
+  if (file) {
     var HTMLfile = new FileReader();
     HTMLfile.onload = function (e) {
+      var parser = new DOMParser();
       contents = e.target.result;
+      //contents = parser.parseFromString(contents, "text/html");
       document.getElementById("ReadResult").innerHTML = contents;
     }
-    HTMLfile.readAsText(f);
+    HTMLfile.readAsText(file);
   } else {
     alert("Failed to load file");
   }
@@ -30,11 +31,17 @@ function readHTML(evt) {
 // }
 
 function extractTags() {
-  var contentHTML = document.getElementById("ReadResult");
-  anchors = contentHTML.getElementsByTagName('a');
-  paras = contentHTML.getElementsByTagName('p');
-  images = contentHTML.getElementsByTagName('img');
-  showTagContents();
+  if (file == undefined) {
+    alert("Choose a file before");
+  } else {
+    var contentHTML = document.getElementById("ReadResult");
+    anchors = contentHTML.getElementsByTagName('a');
+    paras = contentHTML.getElementsByTagName('p');
+    images = contentHTML.getElementsByTagName('img');
+    showTagContents();
+    document.getElementById("resultsWindow").style.display = "block";
+  }
+
 }
 
 function showTagContents() {
@@ -60,6 +67,12 @@ function showTagContents() {
 }
 
 function modifyEmail () {
+
+  var modHrefsValues = [];
+  var modSrcsValues = [];
+  var modAltsValues = [];
+  var modTitlesValues = [];
+
 
   function getNewValues () {
     // This functions get the value of the input forms showing at the right column. It stores them in an array, the global variables named modXXXXValues.
@@ -91,34 +104,88 @@ function modifyEmail () {
     anchors[i].href = modHrefsValues[i];
   }
 
-  for (var j = 0; j < anchors.length; j++) {
+  for (var j = 0; j < images.length; j++) {
     images[j].src = modSrcsValues[j];
   }
 
-  for (var k = 0; k < anchors.length; k++) {
-    anchors[k].alt = modAltsValues[k];
+  for (var k = 0; k < images.length; k++) {
+    images[k].alt = modAltsValues[k];
   }
 
-  for (var h = 0; h < anchors.length; h++) {
-    anchors[h].title = modTitlesValues[h];
+  for (var h = 0; h < images.length; h++) {
+    images[h].title = modTitlesValues[h];
   }
+
 }
 
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/À/g, "&Agrave;")
+    .replace(/à/g, "&agrave;")
+    .replace(/Â/g, "&Acirc;")
+    .replace(/â/g, "&acirc;")
+    .replace(/Æ/g, "&AElig;")
+    .replace(/æ/g, "&aelig;")
+    .replace(/Ç/g, "&Ccedil;")
+    .replace(/ç/g, "&ccedil;")
+    .replace(/È/g, "&Egrave;")
+    .replace(/è/g, "&egrave;")
+    .replace(/É/g, "&Eacute;")
+    .replace(/é/g, "&eacute;")
+    .replace(/Ê/g, "&Ecirc;")
+    .replace(/ê/g, "&ecirc;")
+    .replace(/Ë/g, "&Euml;")
+    .replace(/ë/g, "&euml;")
+    .replace(/Î/g, "&Icirc;")
+    .replace(/î/g, "&icirc;")
+    .replace(/Ï/g, "&Iuml;")
+    .replace(/ï/g, "&iuml;")
+    .replace(/Ô/g, "&Ocirc;")
+    .replace(/ô/g, "&ocirc;")
+    .replace(/Œ/g, "&OElig;")
+    .replace(/œ/g, "&oelig;")
+    .replace(/Ù/g, "&Ugrave;")
+    .replace(/ù/g, "&ugrave;")
+    .replace(/Û/g, "&Ucirc;")
+    .replace(/û/g, "&ucirc;")
+    .replace(/Ü/g, "&Uuml;")
+    .replace(/ü/g, "&uuml;")
+
+    .replace(/«/g, "&laquo;")
+    .replace(/»/g, "&raquo;")
+    .replace(/’/g, "&rsquo;")
+    .replace(/®/g, "&reg;")
+    .replace(/•/g, "&bull;")
+    .replace(/Ω/g, "&Omega;")
+    .replace(/†/g, "&dagger;")
+    .replace(/‡/g, "&ddagger;")
+    .replace(/©/g, "&copy;")
+    
+    .replace(/<tbody>/g, " ")
+    .replace(/<\/tbody>/g, " ");
+  }
+
 function saveAsFile() {
-  var textToSave = document.getElementById("ReadResult").innerHTML;
-  var textToSaveAsBlob = new Blob([textToSave], {type: "text/html"});
-  var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-  var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+  if (file == undefined) {
+    alert("Choose a file before");
+  } else {
+    textToSave = document.getElementById("ReadResult").innerHTML;
+    textToSave = escapeHtml(textToSave);
+    // var textToSave = contents;
+    var textToSaveAsBlob = new Blob([textToSave], {encoding:"UTF-8", type:"text/html;charset=UTF-8"});
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+  
+    var downloadLink = document.createElement("a");
+    downloadLink.download = file.name;
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = textToSaveAsURL;
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  
+    downloadLink.click();
+  }
 
-  var downloadLink = document.createElement("a");
-  downloadLink.download = fileNameToSaveAs;
-  downloadLink.innerHTML = "Download File";
-  downloadLink.href = textToSaveAsURL;
-  downloadLink.onclick = destroyClickedElement;
-  downloadLink.style.display = "none";
-  document.body.appendChild(downloadLink);
-
-  downloadLink.click();
 }
 
 function destroyClickedElement(event) {
